016e72ab92f1d14af888a621223a7d5a3caefbbb
morux2 <k-morimoto@keio.jp>
2019-05-25 10:30:44 +0900

    insertSort

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,5 +1,36 @@
 public class Anagram {
-	public void main(String args[]) {
 
+	public static void main(String[] args) {
+		String question = "";
+		for (String word : args)
+			question += word;
+		System.out.println(question);
+		question = insertSort(question);
+		System.out.println(question);
+	}
+
+	//参照のコピーが渡るので注意が必要
+	static String insertSort(String word) {
+		//小文字に直す
+		word = word.toLowerCase();
+		//QuはQに置き換える
+		word = word.replace("qu", "q");
+		//char配列に変換
+		char[] chars = word.toCharArray();
+		for (int i = 1; i < chars.length; i++) {
+			int j = i;
+			while (j >= 1 && chars[j - 1] > chars[j]) {
+				//swapする
+				swap(chars, j - 1, j);
+				j--;
+			}
+		}
+		return new String(chars);
+	}
+
+	static void swap(char[] chars, int i, int j) {
+		char tmp = chars[i];
+		chars[i] = chars[j];
+		chars[j] = tmp;
 	}
 }
